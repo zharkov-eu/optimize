f27b4267121ac3fdf119d9497866955d8e695d48
Evgeni Zharkov <zharkov.e.u@gmail.com>
2018-05-15 17:39:02 +0300

    Реализовано снижение размерности пространства и кластеризация

--- glass/main.py
+++ glass/main.py
@@ -31,6 +31,18 @@ glass {
 def linearize_glass(glass):
     return map(lambda prop: float(prop[1]), filter(lambda prop: prop[0] != 'id' and prop[0] != 'Type', glass.items()))
 
+def cluster_info(cluster, linearize_glasses):
+    predictions = cluster.fit_predict(linearize_glasses)
+    predictCluster = {}
+    predictIdToType = {}
+    for (idx, glass) in enumerate(glasses):
+        group = predictions[idx]
+        if predictCluster.get(group) == None:
+            predictCluster[group] = []
+        predictCluster[group].append(glass.get('id'))
+        predictIdToType[glass.get('id')] = group
+    return {'predictCluster': predictCluster, 'predictIdToType': predictIdToType}
+
 with open(os.path.join('..', 'data', 'glass.csv'), 'rb') as csv_file:
     reader = csv.reader(csv_file)
     for (num, line) in enumerate(reader):
@@ -57,84 +69,103 @@ X = np.array(map(lambda glass: linearize_glass(glass), glasses))
 
 # Кластеризация (K-средних)
 kmeans = KMeans(n_clusters=7).fit(X)
-kmeansCluster = {}
-kmeansIdToType = {}
-for glass in glasses:
-    group = kmeans.predict([linearize_glass(glass), linearize_glass(glass)])[0]
-    if kmeansCluster.get(group) == None:
-        kmeansCluster[group] = []
-    kmeansCluster[group].append(glass.get('id'))
-    kmeansIdToType[glass.get('id')] = group
+kmeansCluster = cluster_info(kmeans, X)
 
 # Кластеризация (AffinityPropagation)
 affinity = AffinityPropagation().fit(X)
-affinityCluster = {}
-affinityIdToType = {}
-for glass in glasses:
-    group = affinity.predict([linearize_glass(glass), linearize_glass(glass)])[0]
-    if affinityCluster.get(group) == None:
-        affinityCluster[group] = []
-    affinityCluster[group].append(glass.get('id'))
-    affinityIdToType[glass.get('id')] = group
+affinityCluster = cluster_info(affinity, X)
 
 # Кластеризация (DBSCAN)
 dbscan = DBSCAN().fit(X)
-dbscanCluster = {}
-dbscanIdToType = {}
+dbscanCluster = cluster_info(dbscan, X)
 
 # Сравнение количества элементов по кластерам
 print('Real:')
 print(map(lambda cluster: (cluster[0], len(cluster[1])), realCluster.items()))
 print('KMeans:')
-print(map(lambda cluster: (cluster[0] + 1, len(cluster[1])), kmeansCluster.items()))
+print(map(lambda cluster: (cluster[0], len(cluster[1])), kmeansCluster.get('predictCluster').items()))
 print('AffinityCluster:')
-print(map(lambda cluster: (cluster[0] + 1, len(cluster[1])), affinityCluster.items()))
-print('DBSCAN:')
-print(map(lambda cluster: (cluster[0] + 1, len(cluster[1])), kmeansCluster.items()))
+print(map(lambda cluster: (cluster[0], len(cluster[1])), affinityCluster.get('predictCluster').items()))
+print('DBSCAN Cluster:')
+print(map(lambda cluster: (cluster[0], len(cluster[1])), dbscanCluster.get('predictCluster').items()))
 
-kmeansErrorCount = 0
+# Снижение размерности пространства:
+realClassification = map(lambda idToType: idToType[1], realIdToType.items())
+kmeansClassification = map(lambda idToType: idToType[1], kmeansCluster.get('predictIdToType').items())
+affinityClassification = map(lambda idToType: idToType[1], affinityCluster.get('predictIdToType').items())
+dbscanClassification = map(lambda idToType: idToType[1], dbscanCluster.get('predictIdToType').items())
 
-# Снижение размерности пространства: SVD
-# X_truncated = TruncatedSVD(n_components=3).fit_transform(X)
+# Снижение размерности пространства: TruncatedSVD
+X_truncated = TruncatedSVD(n_components=3).fit_transform(X)
 
-# fig = plt.figure(figsize=(16, 8))
+trunk_fig = plt.figure()
+ax = trunk_fig.add_subplot(111, projection='3d')
+ax.scatter(X_truncated[:, 0], X_truncated[:, 1], X_truncated[:, 2], c=realClassification)
+ax.set_title("Real Clustering: TruncatedSVD reduction (3d) of (9d) data")
 
-# ax = fig.add_subplot(121, projection='3d')
-# ax.scatter(X_truncated[:, 0], X_truncated[:, 1], X_truncated[:, 2], c=map(lambda idToType: idToType[1], realIdToType.items()))
-# ax.set_title("Real Clustering: Truncated SVD reduction (2d) of (9d) data")
+trunk_fig_kmean = plt.figure()
+ax = trunk_fig_kmean.add_subplot(111, projection='3d')
+ax.scatter(X_truncated[:, 0], X_truncated[:, 1], X_truncated[:, 2], c=kmeansClassification)
+ax.set_title("KMeans Clustering: TruncatedSVD reduction (3d) of (9d) data")
 
-# ax = fig.add_subplot(122, projection='3d')
-# ax.scatter(X_truncated[:, 0], X_truncated[:, 1], X_truncated[:, 2], c=map(lambda idToType: idToType[1], kmeansIdToType.items()))
-# ax.set_title("KMeans Clustering: Truncated SVD reduction (2d) of (9d) data")
+trunk_fig_affinity = plt.figure()
+ax = trunk_fig_affinity.add_subplot(111, projection='3d')
+ax.scatter(X_truncated[:, 0], X_truncated[:, 1], X_truncated[:, 2], c=affinityClassification)
+ax.set_title("Affinity Clustering: TruncatedSVD reduction (3d) of (9d) data")
 
-# plt.show()
+trunk_fig_dbscan = plt.figure()
+ax = trunk_fig_dbscan.add_subplot(111, projection='3d')
+ax.scatter(X_truncated[:, 0], X_truncated[:, 1], X_truncated[:, 2], c=dbscanClassification)
+ax.set_title("DBSCAN Clustering: TruncatedSVD reduction (3d) of (9d) data")
+
+plt.show()
 
 # Снижение размерности пространства: Pricipal Component Analysis
-X_pca = PCA(n_components=3).fit_transform(X)
+X_factor = PCA(n_components=3).fit_transform(X)
 
-fig = plt.figure(figsize=(16, 8))
+pca_fig = plt.figure()
+ax = pca_fig.add_subplot(111, projection='3d')
+ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=realClassification)
+ax.set_title("Real Clustering: PCA reduction (3d) of (9d) data")
 
-ax = fig.add_subplot(121, projection='3d')
-ax.scatter(X_pca[:, 0], X_pca[:, 1], X_pca[:, 2], c=map(lambda idToType: idToType[1], realIdToType.items()))
-ax.set_title("Real Clustering: PCA reduction (2d) of (9d) data")
+pca_fig_kmean = plt.figure()
+ax = pca_fig_kmean.add_subplot(111, projection='3d')
+ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=kmeansClassification)
+ax.set_title("KMeans Clustering: PCA reduction (3d) of (9d) data")
 
-ax = fig.add_subplot(122, projection='3d')
-ax.scatter(X_pca[:, 0], X_pca[:, 1], X_pca[:, 2], c=map(lambda idToType: idToType[1], kmeansIdToType.items()))
-ax.set_title("KMeans Clustering: PCA reduction (2d) of (9d) data")
+pca_fig_affinity = plt.figure()
+ax = pca_fig_affinity.add_subplot(111, projection='3d')
+ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=affinityClassification)
+ax.set_title("Affinity Clustering: PCA reduction (3d) of (9d) data")
+
+pca_fig_dbscan = plt.figure()
+ax = pca_fig_dbscan.add_subplot(111, projection='3d')
+ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=dbscanClassification)
+ax.set_title("DBSCAN Clustering: PCA reduction (3d) of (9d) data")
 
 plt.show()
 
 # Снижение размерности пространства: Factor Analysis
-# X_factor = FactorAnalysis(n_components=3).fit_transform(X)
+X_factor = FactorAnalysis(n_components=3).fit_transform(X)
 
-# fig = plt.figure(figsize=(16, 8))
+factor_fig = plt.figure()
+ax = factor_fig.add_subplot(111, projection='3d')
+ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=realClassification)
+ax.set_title("Real Clustering: Factor Analysis reduction (3d) of (9d) data")
 
-# ax = fig.add_subplot(121, projection='3d')
-# ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=map(lambda idToType: idToType[1], realIdToType.items()))
-# ax.set_title("Real Clustering: Factor Analysis reduction (2d) of (9d) data")
+factor_fig_kmean = plt.figure()
+ax = factor_fig_kmean.add_subplot(111, projection='3d')
+ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=kmeansClassification)
+ax.set_title("KMeans Clustering: Factor Analysis reduction (3d) of (9d) data")
 
-# ax = fig.add_subplot(122, projection='3d')
-# ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=map(lambda idToType: idToType[1], kmeansIdToType.items()))
-# ax.set_title("KMeans Clustering: Factor Analysis reduction (2d) of (9d) data")
+factor_fig_affinity = plt.figure()
+ax = factor_fig_affinity.add_subplot(111, projection='3d')
+ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=affinityClassification)
+ax.set_title("Affinity Clustering: Factor Analysis reduction (3d) of (9d) data")
 
-# plt.show()
+factor_fig_dbscan = plt.figure()
+ax = factor_fig_dbscan.add_subplot(111, projection='3d')
+ax.scatter(X_factor[:, 0], X_factor[:, 1], X_factor[:, 2], c=dbscanClassification)
+ax.set_title("DBSCAN Clustering: Factor Analysis reduction (3d) of (9d) data")
+
+plt.show()
